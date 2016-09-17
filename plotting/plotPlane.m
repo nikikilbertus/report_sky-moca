dad4351fd1c92f57c4f40f33497358405cdf30e5
nikikilbertus <kilbertus.niki@gmail.com>
2016-09-17 18:34:03 +0200

    removed old matlab plotting functions

--- plotting/plotPlane.m
+++
@@ -1,7 +0,0 @@
-plane = 'yz';
-if plane == 'yz'
-    for i = 1:N
-        quiver3(X(i,:,:),Y(i,:,:),Z(i,:,:),U(i,:,:),V(i,:,:),W(i,:,:),'color',[1,0,0]); axis equal; shg
-        pause;
-    end
-end
